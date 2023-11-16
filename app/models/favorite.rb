7b281c82d9b4d0418419daf06e9c1f2221434f80
Rodolfo Machado <machado.rodolfo@outlook.com>
2023-11-16 09:22:53 -0300

    Preparando os Models

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,5 @@
 class Favorite < ApplicationRecord
   belongs_to :favoritable, polymorphic: true
   belongs_to :user
+  validates :user_id, uniqueness: { scope: [:favoritable_type, :favoritable_id], message: "can favorite only on time per resource"}
 end
